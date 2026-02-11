7a52d9fb044b6a89ccef69cc4a39564989a5ae29
Kumar-Adarsh-Singh <kumaradarshsingh131@gmail.com>
2026-02-11 23:30:36 +0530

    added event model

---
+++ src/Models/event.model.js
@@ -0,0 +1,31 @@
+import mongoose, {model, Schema} from "mongoose";
+
+const eventSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  }
+}, {timestamps: true});
+
+export const Event = model("Event", eventSchema);
